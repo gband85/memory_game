5ca0e9f17b4b83a5cb712517d8b1e16af071dd07
gband85 <garrett@gband.net>
2022-07-14 14:06:55 -0500

    import images programmatically

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import logo from './logo.svg';
-import am_red_squirrel from './Tamiasciurus_hudsonicus_CT.jpg';
-import black_squirrel from './Black_squirrel_in_Santa_Clara_CA.jpg'
-import eastern_gray_squirrel from './EasternGraySquirrel_GAm.jpg'
 import './App.css';
 import Card from './Card';
 
 const App=()=> {
+  const importAll=(r)=>{
+    return r.keys();
+  }
+  const images=importAll(require.context('./images', false, /\.(png|jpe?g|svg)$/));
   return (
-    <div>
-<Card src={am_red_squirrel}/>
-<Card src={black_squirrel}/>
-<Card/>
+    <div className='cards'>
+    {images.map((img)=>{      
+      return <Card src={require('./images/'+ img.replace('./',''))}/>
+    })}
 </div>
   );
 }
