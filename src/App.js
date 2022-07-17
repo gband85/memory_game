7bcc07169e202a57fc3799c485fc37e563331909
gband85 <garrett@gband.net>
2022-07-17 13:19:57 -0500

    pass image path and animal names to card component

--- src/App.js
+++ src/App.js
@@ -8,8 +8,10 @@ const App=()=> {
   const images=importAll(require.context('./images', false, /\.(png|jpe?g|svg)$/));
   return (
     <div className='cards'>
-    {images.map((img)=>{      
-      return <Card src={require('./images/'+ img.replace('./',''))}/>
+    {images.map((img)=>{
+      const common_name=img.slice(2,img.search(/-/g)).replace(/_/g," ")
+      const sci_name=img.slice(img.search(/-/g)+1,img.length-4).replace(/_/g," ")
+      return <Card common_name={common_name} sci_name={sci_name} src={require('./images/'+img.replace('./',''))}/>
     })}
 </div>
   );
