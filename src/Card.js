69412acd70b4b93363e1e78997156a2c61ed560e
gband85 <garrett@gband.net>
2022-07-17 13:20:38 -0500

    take props and output in component

--- src/Card.js
+++ src/Card.js
@@ -1,10 +1,16 @@
-import React, {useState} from "react";
+import React, {useState, createRef} from "react";
 
 const Card=(props)=>{
+    const imgRef=createRef();
     const [clicked, setClicked]=useState(false);
 return (
-    <div>
-        <img src={props.src} onClick={()=>setClicked(true)} alt=""></img>
+    <div className="card" onClick={()=>setClicked(true)}>
+    
+        <img src={props.src} alt="" height={750} width={600}></img>
+        <div>
+        <p>{props.common_name}</p>
+        <p>{props.sci_name}</p>
+        </div>
     </div>
 )
 }
