1f44a4ebfc5f4cd4a3390946173ac47c04893438
gband85 <garrett@gband.net>
2022-07-21 13:27:51 -0500

    alter clicked prop

--- src/Card.js
+++ src/Card.js
@@ -1,10 +1,18 @@
 import React, {useState, createRef} from "react";
 
 const Card=(props)=>{
-    const imgRef=createRef();
-    const [clicked, setClicked]=useState(false);
+    const handleClick=()=>{
+        if (props.clicked===false) {
+        props.updateClickedCards(props.id)
+        props.updateScore();
+        }
+        else {
+            //tell game to restart
+            props.resetGame();
+        }
+    }
 return (
-    <div className="card" onClick={()=>setClicked(true)}>
+    <div className="card" onClick={()=>handleClick()}>
     
         <img src={props.src} alt="" height={750} width={600}></img>
         <div>
