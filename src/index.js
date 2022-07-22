0bbc60c3bed6ea49cdaee08926a75cf57ccc2ab8
gband85 <garrett@gband.net>
2022-07-21 20:43:25 -0500

    add navbar

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+        <div class="navbar">
+    <h1 class="navbar__brand">Squirrel Memory Game</h1>
+</div>
     <App />
   </React.StrictMode>
 );
